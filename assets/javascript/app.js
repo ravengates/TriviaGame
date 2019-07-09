c02c3cfeb1b85624bc64a3af4b855a92874760bb
Raven Gates <ravengates@icloud.com>
2019-07-09 12:12:36 -0500

    bootstrap buttons added, basic html skeleton added, psuedo coded javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// when website first opens, only Trivia Game and Start Button should show
+// hide Timer, Form-( Trivia Questions and done Button), and Results Page
+
+//Once Start Button is clicked 
+//timer is set for 30 seconds and begins counting down-- when at 0 -shows results
+//Triva Questions are shown// only able to chose one
+
+
+//Once Timer is Out OR when done clicked
+//Result Page is shown 
