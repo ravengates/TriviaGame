3e3c65d4fc716010c5f28776f93be12bd4f36106
Raven Gates <ravengates@icloud.com>
2019-07-09 19:23:10 -0500

    css, html updated. onclick events, variables, and some functions created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,68 @@
 
 //Once Timer is Out OR when done clicked
 //Result Page is shown 
+
+//variables needed------------------------------------
+var seconds = 30;
+var questionsCorrect = 0;
+var questionsWrong = 0;
+var questionsNotAnswered= 0;
+var intervalId;
+
+// when the website first opens ////Hide Function
+////hide timer, question form, and result page
+$(document).ready(function() {
+    $('#questions').hide();
+    $('#timer').hide();
+    $('#results').hide();
+
+
+//FUNCTIONS-----------------------------------------------
+
+//show functions.
+function unHide(){
+    $('#questions').show();
+    $('#timer').show();
+
+}
+ 
+//hide function.
+function hide(){
+    $('#questions').hide();
+    $('#timer').hide();
+       
+}
+
+//Show Results function 
+function showResults(){
+    $('#results').show();
+
+}
+
+//TIMER FUNCTIONS-----------------------------------------
+
+
+//CLICK EVENTS---------------------------------------------
+
+//click start button --
+$('#startBtn').on('click', function(){
+    $('#startBtn').hide();
+    unHide();
+    
+
+});
+
+//click done button
+$('#doneBtn').on('click',function(){
+    $('#doneBtn').hide();
+    hide();
+    showResults();
+    
+});
+
+
+
+
+});
+
+
